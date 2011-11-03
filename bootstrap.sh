78ade0e2f4825b4771986f81420c09248be7fd22
Pete Brousalis <pbrousalis@enovafinancial.com>
2011-11-02 21:36:30 -0400

    Added gitconfig

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,4 +5,4 @@ ln -sf  $HOME/dotfiles/vimrc $HOME/.vimrc
 ln -sf  $HOME/dotfiles/bashrc $HOME/.bashrc
 ln -sf  $HOME/dotfiles/vim $HOME/.vim
 ln -sf  $HOME/dotfiles/screenrc $HOME/.screenrc
-#ln -sf  $HOME/dotfiles/gitconfig $HOME/.gitconfig
+ln -sf  $HOME/dotfiles/gitconfig $HOME/.gitconfig

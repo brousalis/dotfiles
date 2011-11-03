4c2b2d5f9472e81ed04a31f517739c42231d6968
cnuapp <cnuapp@asterisk.cashnetusa.com>
2011-11-02 20:20:33 -0400

    Forgot to delete files

--- vim/ftdetect/plaintext.vim
+++
@@ -1,7 +0,0 @@
-" This is the autodetect file for the plain text filetype which is used
-" generally for editing prose.
-
-augroup plaintext 
-     au! BufRead,BufNewFile *.txt   setfiletype plaintext 
-augroup END
-
--- zsh-custom/permissions.zsh
+++
@@ -1,6 +0,0 @@
-# chmod and permissions commands
-alias mx='chmod a+x'
-alias 000='chmod 000'
-alias 644='chmod 644'
-alias 755='chmod 755'
-alias perm='stat --printf "%a %n \n "' # requires a file name e.g. perm file

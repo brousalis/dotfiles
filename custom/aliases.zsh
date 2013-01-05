4ea63d33ef1617b8b62723f360a22bfd58e861af
brousalis <brousapg@gmail.com>
2013-01-05 17:16:22 -0600

    Better rakefile

--- custom/aliases.zsh
+++ custom/aliases.zsh
@@ -1,6 +1,7 @@
 alias ..='cd ..'
 alias ...='cd ../..'
 alias ....='cd ../../..'
+alias .....='cd ../../../..'
 function u() { NUM=${1:-1}; for (( start = 1; start <= $NUM; start++ )); do cd ..; done; }
 
 # aliases
@@ -10,7 +11,7 @@ alias dots='cd ~/dotfiles'
 alias r='rails'
 alias ifi='ifconfig | ack "net"'
 alias untar="tar -xvvf"
-alias reload="source ~/.bashrc"
+alias reload="source ~/.zshrc.local"
 alias dsremove="find . -name .DS_Store -print0 | xargs -0 git rm --ignore-unmatch"
 alias server='open http://localhost:1337 && python -m SimpleHTTPServer 1337'
 alias rails_new='rails new $1 -m https://raw.github.com/RailsApps/rails-composer/master/composer.rb'
@@ -35,31 +36,17 @@ alias lsx='ls -ax'                # sort right to left rather then in columns
 alias lss='ls -shAxSr'            # sort by size
 alias lt='ls -lAtrh'              # sort by date and human readable
 alias lm='ls -al |more'           # pipe through 'more'
-alias lls='ls -l -h -g -F --color=auto'
 alias lc='ls -aCF'
 
-# pretty ways to look at directory contents
 alias l1='tree --dirsfirst -ChFL 1'
 alias l2='tree --dirsfirst -ChFL 2'
 alias l3='tree --dirsfirst -ChFL 3'
 
-alias ll1='tree --dirsfirst -ChFupDaL 1'
-alias ll2='tree --dirsfirst -ChFupDaL 2'
-alias ll3='tree --dirsfirst -ChFupDaL 3'
-
-alias l='l1'
-alias ll='ll1'
+alias l='ls -A'
+alias ll='l1'
 
 # Remove autocorrect for some terms
 alias mv="nocorrect mv"
 alias cp="nocorrect cp"
 alias mkdir="nocorrect mkdir"
 alias tmux="nocorrect tmux"
-
-function vmstart() {
-  VBoxHeadless --startvm "$1" &
-}
-
-function vmstop {
-  VBoxManage controlvm "$1" savestate
-} 
